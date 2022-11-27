a56263410780b9f2afcecae490ca3eeb030a8bce
Joseph Herrera <josephherreradev@outlook.com>
2022-11-27 17:39:53 -0600

    Guardado a base de datos

--- eventos.php
+++ eventos.php
@@ -1,86 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-      crossorigin="anonymous"
-    />
-    <link href="style.css" rel="stylesheet" />
-    <title>Document</title>
-  </head>
-  <body>
-    <!--Navegacion-->
-    <div id="nav-placeholder"></div>
-    <script>
-      $(function () {
-        $("#nav-placeholder").load("header.html");
-      });
-    </script>
-    <!-- Section that adds events inside a form that works with bootstrap -->
-    <main>
-      <div class="contenedor">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-12">
-              <h1 class="text-center">Eventos</h1>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-12">
-              <form action="eventos.php" method="post">
-                <div class="mb-3">
-                  <label for="nombre" class="form-label">Nombre</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="nombre"
-                    name="nombre"
-                    placeholder="Nombre del evento"
-                  />
-                </div>
-                <div class="mb-3">
-                  <label for="descripcion" class="form-label"
-                    >Descripcion</label
-                  >
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="descripcion"
-                    name="descripcion"
-                    placeholder="Descripcion del evento"
-                  />
-                </div>
-                <div class="mb-3">
-                  <label for="fecha" class="form-label">Fecha</label>
-                  <input
-                    type="date"
-                    class="form-control"
-                    id="fecha"
-                    name="fecha"
-                    placeholder="Fecha del evento"
-                  />
-                </div>
-                <div class="mb-3">
-                  <label for="hora" class="form-label">Hora</label>
-                  <input
-                    type="time"
-                    class="form-control"
-                    id="hora"
-                    name="hora"
-                    placeholder="Hora del evento"
-                  />
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  </body>
-</html>
+<?php
+    if ($_POST) {
+        try {
+            $conexion = new PDO('mysql:host=localhost;dbname=agenda', 'root', '');
+            $nombre = $_POST['nombre'];
+            $descripcion = $_POST['descripcion'];
+            $fecha = $_POST['fecha'];
+            $hora = $_POST['hora'];
+
+            session_start();
+
+            $consulta = $conexion->prepare('INSERT INTO evento VALUES (NULL, :nombre, :descripcion, :fecha, :hora)');    
+    
+            $consulta->execute(array(":nombre" => $nombre, ":descripcion" => $descripcion, ":fecha" => $fecha, ":hora" => $hora));
+            header('Location: eventos.html');
+
+        } catch (PDOException $e) {
+            echo "Vale madre: " . $e->getMessage();
+        }
+    }
+    else{
+        echo "Chinga tu madre";
+    }
+?>
+
--- notas.php
+++ notas.php
@@ -1,71 +1,31 @@
 <?php
     if ($_POST) {
         try {
+            $conexion = new PDO('mysql:host=localhost;dbname=agenda', 'root', '');
             $nombre = $_POST['nombre-nota'];
             $nota = $_POST['nota'];
 
-            $conexion = new PDO('mysql:host=localhost;dbname=tienda', 'root', '');
-            $consulta = $conexion->prepare('INSERT INTO usuario VALUES (NULL, :nombre, :nota)');    
-    
-            $consulta->execute(array(":nombre" => $nombre, ":nota" => $nota));
-            header('Location: calendario.php');
+            session_start();
+            $usuario = $_SESSION["usuario"];
+            $query="SELECT * FROM usuario WHERE Nombre = '".$usuario."'";
             
+            $resultado1 = $conexion->query($query);
+            $resultado1->execute();
+
+            foreach($resultado1 as $fila){
+                $idUsuario = $fila['idUsuario'];
+            }
+
+            $consulta = $conexion->prepare('INSERT INTO nota VALUES (NULL, :nombre, :nota, :idUsuario)');    
+    
+            $consulta->execute(array(":nombre" => $nombre, ":nota" => $nota, ":idUsuario" => $idUsuario));
+            header('Location: notas.html');
+
         } catch (PDOException $e) {
             echo "Vale madre: " . $e->getMessage();
         }
     }
+    else{
+        echo "Chinga tu madre";
+    }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-      crossorigin="anonymous"
-    />
-    <link href="style.css" rel="stylesheet" />
-    <title>Agenda Mamamlona</title>
-  </head>
-
-  <body>
-    <!--Navegacion-->
-    <div id="nav-placeholder"></div>
-    <script>
-      $(function () {
-        $("#nav-placeholder").load("header.html");
-      });
-    </script>
-    <main>
-      <div class="contenedor">
-        <div class="container-fluid">
-          <h2>Ingrese su nota</h2>
-          <form
-            action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>"
-            method="post"
-          >
-            <div class="input-group mb-3">
-              <span class="input-group-text">Nombre</span>
-              <input id="nombre-nota" type="text" class="form-control" />
-            </div>
-            <div class="input-group">
-              <textarea
-                id="nota"
-                class="form-control"
-                aria-label="With textarea"
-                rows="15"
-              ></textarea>
-            </div>
-            <button type="button boton" class="btn btn-outline-primary">
-              Guardar
-            </button>
-          </form>
-        </div>
-      </div>
-    </main>
-  </body>
-</html>
