514ac90040d3ca1950f6cb8deb5a4501484487f2
Joseph Herrera <josephherreradev@outlook.com>
2022-11-27 18:33:58 -0600

    Final

--- agenda.php
+++ agenda.php
@@ -1,5 +1,41 @@
 <?php
-$nombre = 'ALV';
+ try {
+  $conexion = new PDO('mysql:host=localhost;dbname=agenda', 'root', '');
+  $nombre_Nota;
+  $Nota;
+  session_start();
+  $usuario = $_SESSION["usuario"];
+
+  $query="SELECT * FROM usuario WHERE Nombre = '".$usuario."'";
+        
+  $resultado1 = $conexion->query($query);
+  $resultado1->execute();
+
+  foreach($resultado1 as $fila){
+      $idUsuario = $fila['idUsuario'];
+  }
+
+  $con = "SELECT Nombre, Nota FROM nota WHERE idUsuario = '".$idUsuario."'"; 
+
+  $consulta = $conexion->query($con); 
+  $consulta->execute(); 
+  
+  foreach($consulta as $fila){
+    $Nota[] = array('Nombre' => $fila['Nombre'], 'Nota' => $fila['Nota']);
+  }
+
+  /* Fill Eventos */
+  $con = "SELECT Nombre, Descripcion, Fecha, Hora FROM evento";
+  $consulta2 = $conexion->query($con);
+  $consulta2->execute();
+
+  foreach($consulta2 as $fila){
+    $Eventos[] = array('Nombre' => $fila['Nombre'], 'Descripcion' => $fila['Descripcion'], 'Fecha' => $fila['Fecha'], 'Hora' => $fila['Hora']);
+  }
+
+} catch (PDOException $e) {
+  echo "Vale madre: " . $e->getMessage();
+}
 ?>
 
 <!DOCTYPE html>
@@ -31,8 +67,23 @@ $nombre = 'ALV';
       <div class="contenedor">
         <div class="container-fluid">
             <!-- Show agenda elements dinamically -->
-            <h3><?php echo $nombre?></h3>
-            </script>
+            <h3>Notas</h3>
+            <p><?php
+                foreach($Nota as $fila){
+                    echo "<h4> Nombre: ".$fila['Nombre']."</h4>";
+                    echo "<p> Nota: ".$fila['Nota']."</p>";
+                    echo "<br>";
+                }
+            ?></p>
+            <h3>Eventos</h3>
+            <p><?php
+                foreach($Eventos as $fila){
+                    echo "<h4> Nombre: ".$fila['Nombre']."</h4>";
+                    echo "<p> Descripcion: ".$fila['Descripcion']."</p>";
+                    echo "<p> Fecha: ".$fila['Fecha']."</p>";
+                    echo "<p> Hora: ".$fila['Hora']."</p>";
+                    echo "<br>";
+                }?>
         </div>
       </div>
     </main>
